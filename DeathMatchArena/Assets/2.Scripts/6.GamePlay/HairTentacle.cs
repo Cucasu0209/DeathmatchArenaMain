082095ebd21ab5b93e6882b7302afb43a2272ead
Trung nq <trung.nq24092000@gmail.com>
2023-05-14 01:08:36 +0700

    Move and Jump Charactor

---
+++ DeathMatchArena/Assets/2.Scripts/6.GamePlay/HairTentacle.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HairTentacle : MonoBehaviour
+{
+    public int length;
+    public LineRenderer lineRend;
+    public Vector3[] segmentPoses;
+    public Vector3[] segmentV;
+
+    public Transform targetDir;
+    public float targetDist;
+    public float smoothSpeed;
+
+    public float wiggleSpeed;
+    public float wiggleMagnitude;
+    public Transform wiggleDir;
+
+    private void Start()
+    {
+        lineRend.positionCount = length;
+        segmentPoses = new Vector3[length];
+        segmentV = new Vector3[length];
+    }
+
+    private void Update()
+    {
+        wiggleDir.localRotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.time * wiggleSpeed) * wiggleMagnitude);
+        segmentPoses[0] = targetDir.position;
+
+        for (int i = 1; i < segmentPoses.Length; i++)
+        {
+            Vector3 targetPos = segmentPoses[i - 1] + (-wiggleDir.up + (segmentPoses[i] - segmentPoses[i - 1])).normalized * targetDist;
+            segmentPoses[i] = Vector3.SmoothDamp(segmentPoses[i], targetPos, ref segmentV[i], smoothSpeed);
+        }
+        lineRend.SetPositions(segmentPoses);
+    }
+}
